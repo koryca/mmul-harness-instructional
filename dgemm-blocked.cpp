a2c451c821dec8236018374aa9af5ba0d905ae00
leonsanbo <kory.zhou@gmail.com>
2021-09-09 15:04:36 -0700

    copy update

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <memory.h>
 const char* dgemm_desc = "Blocked dgemm.";
 
 /* This routine performs a dgemm operation
@@ -13,15 +14,15 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          //copy to local
          // std::vector<double> buf(6 * n * n);
          // double * Clocal = buf.data() + block_size * block_size;
-         double * Clocal = new double[block_size];
+         long double * Clocal = new long double[block_size];
          std::copy(C, C + block_size, Clocal);
          // memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
          for(int k=0; k<n; k+=block_size){
             // C[i*nb+j] += A[i*nb+k] * B[k*nb+j];
             // double * Alocal = Clocal + block_size * block_size;
             // double * Blocal = Alocal + block_size * block_size;
-            double * Alocal = new double[block_size];
-            double * Blocal = new double[block_size];
+            long double * Alocal = new long double[block_size];
+            long double * Blocal = new long double[block_size];
             std::copy(A, A + block_size, Alocal);
             std::copy(B, B + block_size, Blocal);
             // memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
@@ -30,11 +31,12 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
                for (int p=0; p<block_size; p++){
                   for(int l=0; l<block_size; l++){
                      // C[i,j] += A[i,k] * B[k,j]
-                     Clocal[p*block_size+m] += Alocal[p*block_size+l] * B[l*block_size+m];
+                     Clocal[p*block_size+m] += Alocal[p*block_size+l] * Blocal[l*block_size+m];
                   }
                }
             }
-            memcpy((void *)C, (const void *)Clocal, sizeof(double)*block_size*block_size);
+            std::copy(Clocal, Clocal + block_size, C);
+            // memcpy((void *)C, (const void *)Clocal, sizeof(double)*block_size*block_size);
          }
       }
    }
