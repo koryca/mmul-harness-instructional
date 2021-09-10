d0ce1c222cd3a83dd3dbdd91077cc57e6e2fef88
leonsanbo <kory.zhou@gmail.com>
2021-09-09 22:08:20 -0700

    test update

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -13,7 +13,7 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    for (int i=0; i<n; i+=block_size){
       for (int j=0; j<n; j+=block_size){
          //copy to local
-         std::vector<double> buf(3 * block_size * block_size);
+         std::vector<double> buf(3 * n * n);
          double * Clocal = buf.data() + block_size * block_size;
          memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
          for(int k=0; k<n; k+=block_size){
