6529d144b74b775c4d4f9b79ea73268255552792
leonsanbo <kory.zhou@gmail.com>
2021-09-10 00:21:15 -0700

    update 9/10

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -8,7 +8,7 @@ const char* dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   std::vector<double> buf(4 * n * n);
+   std::vector<double> buf(3 * n * n);
    double * Clocal = buf.data() + 0;
    double * Alocal = Clocal + n * n;
    double * Blocal = Alocal + n * n;
@@ -21,11 +21,11 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          for(int k=0; k<n; k+=block_size){
             memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
             memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
-            for (int p=i; p<i+block_size; p++){
-               for (int q=j; q<j+block_size; q++){
-                  for(int m=k; m<k+block_size; m++){
+            for (int ii=i; ii<i+block_size; ii++){
+               for (int jj=j; jj<j+block_size; jj++){
+                  for(int kk=k; kk<k+block_size; kk++){
                      // C[i,j] += A[i,k] * B[k,j]
-                     Clocal[p+q*block_size] += Alocal[p+m*block_size] * Blocal[m+q*block_size];
+                     Clocal[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
                   }
                }
             }
