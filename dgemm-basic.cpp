56f4590a287848981bc75dd6af7b26491e20f0e8
leonsanbo <kory.zhou@gmail.com>
2021-09-09 21:23:14 -0700

    test update

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -9,11 +9,11 @@ const char* dgemm_desc = "Basic implementation, three-loop dgemm.";
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
    // This implementation is the triply nested loop as discussed in class
-   for (int j=0; j<n; j++){
-      for (int i=0; i<n; i++){
+   for (int i=0; i<n; i++){
+      for (int j=0; j<n; j++){
          for(int k=0; k<n; k++){
             // C[i,j] += A[i,k] * B[k,j]
-            C[j*n+i] += A[j*n+k] * B[k*n+i];
+            C[i*n+j] += A[i*n+k] * B[k*n+j];
          }
       }
    }
