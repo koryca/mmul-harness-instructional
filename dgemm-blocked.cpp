376150b06a70a794118ecd82aa9bec237d33436a
leonsanbo <kory.zhou@gmail.com>
2021-09-09 23:17:36 -0700

    test update

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -22,10 +22,12 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
             memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
             for (int p=i; p<i+block_size; p++){
                for (int q=j; q<j+block_size; q++){
+                  double * temp = new double[block_size * block_size];
                   for(int m=k; m<k+block_size; m++){
                      // C[i,j] += A[i,k] * B[k,j]
-                      Clocal[p*block_size+q*block_size] += Alocal[p+m*block_size] * Blocal[m+q*block_size];
+                     temp[p*block_size+q*block_size]  += Alocal[p*block_size+m] * Blocal[m+q*block_size];
                   }
+                  Clocal[p+q*block_size] = temp[p*block_size+q*block_size];
                }
             }
             memcpy((void *)C, (const void *)Clocal, sizeof(double)*block_size*block_size);
