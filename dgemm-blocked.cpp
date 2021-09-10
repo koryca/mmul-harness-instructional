a98cf9dfe8a101f5030bfce1800bf9d4d44cd690
leonsanbo <kory.zhou@gmail.com>
2021-09-10 11:49:46 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -15,11 +15,11 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    double * Blocal = Alocal + n * n;
    
    // test with block_size 2 BxB=2x2 MM size 64x64
-   // i from 0 to n/block_size = 64/2 = 32 times of iteration
-   for (int i=0; i<n/block_size; i++){
-      for (int j=0; j<n/block_size; j++){ //same as i
+   // i from 0 to n, increment by block_size, so n/block_size = 64/2 = 32 times of iteration
+   for (int i=0; i<n; i+=block_size){
+      for (int j=0; j<n; j+=block_size){ //same as i
          memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
-         for(int k=0; k<n/block_size; k++){ // same as i
+         for(int k=0; k<n; k+=block_size){ // same as i
             memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
             memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
             // init ii=i, ii from i to i+block_size ->block_size times of iteration
