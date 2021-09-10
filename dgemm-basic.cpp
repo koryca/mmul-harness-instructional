0c140bcb3c90f519ff8647b0ad364848d9f85795
leonsanbo <kory.zhou@gmail.com>
2021-09-10 01:59:49 -0700

    test update

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -13,7 +13,7 @@ void square_dgemm(int n, double* A, double* B, double* C)
       for (int j=0; j<n; j++){
          for(int k=0; k<n; k++){
             // C[i,j] += A[i,k] * B[k,j]
-            C[i+j*n] += A[i+k*n] * B[k+j*n];
+            C[i + j * n] += A[i + k * n] * B[k + j * n];
          }
       }
    }
