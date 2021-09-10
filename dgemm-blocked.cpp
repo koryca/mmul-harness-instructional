5d3ba02ec68418a3815375876476930723f35fd0
leonsanbo <kory.zhou@gmail.com>
2021-09-10 11:31:02 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -9,35 +9,29 @@ const char* dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   // std::vector<double> buf(3 * n * n);
-   // double * Clocal = buf.data() + 0;
-   // double * Alocal = Clocal + n * n;
-   // double * Blocal = Alocal + n * n;
+   std::vector<double> buf(3 * n * n);
+   double * Clocal = buf.data() + 0;
+   double * Alocal = Clocal + n * n;
+   double * Blocal = Alocal + n * n;
    
-   // int nb = n/block_size;
-   // for (int i=0; i<n/block_size; i++){
-   //    for (int j=0; j<n/block_size; j++){
-   //       //copy to local
-   //       memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
-   //       for(int k=0; k<n/block_size; k++){
-   //          memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
-   //          memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
-   //          for (int ii=i; ii<i+block_size; ii++){
-   //             for (int jj=j; jj<j+block_size; jj++){
-   //                for(int kk=k; kk<k+block_size; kk++){
-   //                   Clocal[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
-   //                }
-   //             }
-   //          }
-   //          memcpy((void *)C, (const void *)Clocal, sizeof(double)*block_size*block_size);
-   //       }
-   //    }
-   // }
-   for (int i=0; i<n; i++){
-      for (int j=0; j<n; j++){
-         for(int k=0; k<n; k++){
-            // C[i,j] += A[i,k] * B[k,j]
-            C[i + j * n] += A[i + k * n] * B[k + j * n];
+   // test with block_size 2 BxB=2x2 MM size 64x64
+   // i from 0 to n/block_size = 64/2 = 32 times of iteration
+   for (int i=0; i<n/block_size; i++){
+      for (int j=0; j<n/block_size; j++){ //same as i
+         memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
+         for(int k=0; k<n/block_size; k++){ // same as i
+            memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
+            memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
+            // init ii=i, ii from i to i+block_size ->block_size times of iteration
+            for (int ii=i; ii<i+block_size; ii++){
+               for (int jj=j; jj<j+block_size; jj++){ //same as ii
+                  for(int kk=k; kk<k+block_size; kk++){ //same as kk
+                      // reference from basic : C[i + j * n] += A[i + k * n] * B[k + j * n];
+                     Clocal[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
+                  }
+               }
+            }
+            memcpy((void *)C, (const void *)Clocal, sizeof(double)*block_size*block_size);
          }
       }
    }
