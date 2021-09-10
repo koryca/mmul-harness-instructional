7efb1f3ec9ec747456a256bd3733b20f8c6f1800
leonsanbo <kory.zhou@gmail.com>
2021-09-10 10:08:05 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -19,16 +19,17 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
       for (int j=0; j<n; j++){
          //copy to local
          // memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
-         // memcpy((void *)temp, (const void *)C, sizeof(double)*block_size*block_size);
+         memcpy((void *)temp, (const void *)C, sizeof(double)*block_size*block_size);
          for(int k=0; k<n; k++){
-            // memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
-            // memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
+            memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
+            memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
             // temp[i + j * n] += A[i + k * n] * B[k + j *n];
             for (int ii=i; ii<i+block_size; ii++){
                for (int jj=j; jj<j+block_size; jj++){
                   for(int kk=k; kk<k+block_size; kk++){
                      // C[i,j] += A[i,k] * B[k,j]
                      C[ii + jj * block_size] += A[ii + kk * block_size] * B[kk + jj * block_size];
+                     temp[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
                      // std::cout << "After: " << Clocal[ii + jj] << " " << Alocal[ii + kk] << " " << Blocal[kk + jj] << std::endl;
                   }
                }
@@ -37,6 +38,6 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          }
       }
    }
-   std::cout << *A << " " << *B << " " << *C << std::endl;
-   // std::cout << *A << " " << *B << " " << *C << " " << *temp << std::endl;
+   // std::cout << *A << " " << *B << " " << *C << std::endl;
+   std::cout << *A << " " << *B << " " << *C << " " << *temp << std::endl;
 }
