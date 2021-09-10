177649d1939cb294e1bfbb7f0a2a031a04a17442
leonsanbo <kory.zhou@gmail.com>
2021-09-10 10:18:24 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -13,24 +13,24 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    double * Clocal = buf.data() + 0;
    double * Alocal = Clocal + n * n;
    double * Blocal = Alocal + n * n;
-   double * temp = Blocal + n * n;
+   // double * temp = Blocal + n * n;
 
-   int nb = n/block_size;
-   for (int i=0; i<n; i+=nb){
-      for (int j=0; j<n; j+=nb){
+   // int nb = n/block_size;
+   for (int i=0; i<n; i+=block_size){
+      for (int j=0; j<n; j+=block_size){
          //copy to local
-         // memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
-         memcpy((void *)temp, (const void *)C, sizeof(double)*block_size*block_size);
-         for(int k=0; k<n; k+=nb){
+         memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
+         // memcpy((void *)temp, (const void *)C, sizeof(double)*block_size*block_size);
+         for(int k=0; k<n; k+=block_size){
             memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
             memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
             // temp[i + j * n] += A[i + k * n] * B[k + j *n];
-            for (int jj=j; jj<j+block_size; jj++){
-               for (int ii=i; ii<i+block_size; ii++){
+            for (int ii=i; ii<i+block_size; ii++){
+               for (int jj=j; jj<j+block_size; jj++){
                   for(int kk=k; kk<k+block_size; kk++){
                      // C[i,j] += A[i,k] * B[k,j]
-                     C[ii + jj * block_size] += A[ii + kk * block_size] * B[kk + jj * block_size];
-                     temp[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
+                     // C[ii + jj * block_size] += A[ii + kk * block_size] * B[kk + jj * block_size];
+                     Clocal[ii + jj * block_size] += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
                      // std::cout << "After: " << Clocal[ii + jj] << " " << Alocal[ii + kk] << " " << Blocal[kk + jj] << std::endl;
                   }
                }
@@ -39,6 +39,6 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          }
       }
    }
-   // std::cout << *A << " " << *B << " " << *C << std::endl;
-   std::cout << *A << " " << *B << " " << *C << " " << *temp << std::endl;
+   std::cout << *A << " " << *B << " " << *C << std::endl;
+   // std::cout << *A << " " << *B << " " << *C << " " << *temp << std::endl;
 }
