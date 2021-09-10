f5fe666bce87d9b3af8394c420f02834324503af
leonsanbo <kory.zhou@gmail.com>
2021-09-10 10:21:22 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -9,22 +9,18 @@ const char* dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   std::vector<double> buf(4 * n * n);
+   std::vector<double> buf(3 * n * n);
    double * Clocal = buf.data() + 0;
    double * Alocal = Clocal + n * n;
    double * Blocal = Alocal + n * n;
-   // double * temp = Blocal + n * n;
-
-   // int nb = n/block_size;
+   
    for (int i=0; i<n; i+=block_size){
       for (int j=0; j<n; j+=block_size){
          //copy to local
          memcpy((void *)Clocal, (const void *)C, sizeof(double)*block_size*block_size);
-         // memcpy((void *)temp, (const void *)C, sizeof(double)*block_size*block_size);
          for(int k=0; k<n; k+=block_size){
             memcpy((void *)Alocal, (const void *)A, sizeof(double)*block_size*block_size);
             memcpy((void *)Blocal, (const void *)B, sizeof(double)*block_size*block_size);
-            // temp[i + j * n] += A[i + k * n] * B[k + j *n];
             for (int ii=i; ii<i+block_size; ii++){
                for (int jj=j; jj<j+block_size; jj++){
                   for(int kk=k; kk<k+block_size; kk++){
@@ -37,5 +33,4 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
       }
    }
    std::cout << *A << " " << *B << " " << *C << std::endl;
-   // std::cout << *A << " " << *B << " " << *C << " " << *temp << std::endl;
 }
