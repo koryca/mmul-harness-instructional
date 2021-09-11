30dc88ececbba0127cbdf5c53143e73c636ef0b0
leonsanbo <kory.zhou@gmail.com>
2021-09-11 15:16:35 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -24,9 +24,9 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          //copy C
          for(int ic = i; ic < i + block_size; ic++){
             for(int jc = j; jc < j + block_size; jc++){
-               memcpy(&Clocal[ic + jc * block_size], &C[ic + jc * block_size], sizeof(double)*block_size*block_size);
-               std::cout << "Clocal at copy: " << Clocal[ic + jc * block_size] 
-                        << " C at copy: " << C[ic + jc * block_size] 
+               memcpy(&Clocal[ic + jc * n], &C[ic + jc * n], sizeof(double)*block_size*block_size);
+               std::cout << "Clocal at copy: " << Clocal[ic + jc * n] 
+                        << " C at copy: " << C[ic + jc * n] 
                         << " C[" << ic << "][" << jc << "]"<< std::endl;
             }
          }
@@ -66,7 +66,7 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          //write back to C
          for(int iic = i; iic < i + block_size; iic++){
             for(int jjc = j; jjc < j + block_size; jjc++){
-               memcpy(&C[iic + jjc * block_size], &Clocal[iic + jjc * block_size], sizeof(double)*block_size*block_size);
+               memcpy(&C[iic + jjc * n], &Clocal[iic + jjc * n], sizeof(double)*block_size*block_size);
             // std::cout << "Clocal at write: " << Clocal[iic + j * block_size] 
             //           << "C at write: " << C[iic + j * block_size] << std::endl;      
             }
