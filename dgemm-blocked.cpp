6872e65e132361559e6b0567bfd97272dda2b16d
leonsanbo <kory.zhou@gmail.com>
2021-09-11 14:54:47 -0700

    test

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -51,7 +51,7 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
             }  
             for (int ii=i; ii<i+block_size; ii++){
                for (int jj=j; jj<j+block_size; jj++){ 
-                  double temp = 0.0;
+                  double temp = Clocal[ii + jj * block_size];
                   for(int kk=k; kk<k+block_size; kk++){ 
                      temp += Alocal[ii + kk * block_size] * Blocal[kk + jj * block_size];
                      std::cout << "A[" << ii << "][" << kk << "] " << Alocal[ii + kk * block_size] 
@@ -59,7 +59,7 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
                               << " ->" << temp << " += " << Alocal[ii + kk * block_size] 
                                                << " * " << Blocal[kk + jj * block_size] << std::endl;
                   }
-                  Clocal[ii + jj * n] = temp;
+                  Clocal[ii + jj * block_size] = temp;
                }
             }
          }
