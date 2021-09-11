677c478d2eae541f021768a349d9ff5a2d168479
leonsanbo <kory.zhou@gmail.com>
2021-09-11 13:18:57 -0700

    debug

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -10,14 +10,14 @@ const char* dgemm_desc = "Blocked dgemm.";
 
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   // std::vector<double> buffer(4 * n * n);
-   // double * Alocal = buffer.data() + 0;
-   // double * Blocal = Alocal + n * n;
-   // double * Clocal = Blocal + n * n;
+   std::vector<double> buffer(3 * n * n);
+   double * Alocal = buffer.data() + 0;
+   double * Blocal = Alocal + n * n;
+   double * Clocal = Blocal + n * n;
         
-   double * Alocal = new double[block_size * block_size];
-   double * Blocal = new double[block_size * block_size];
-   double * Clocal = new double[block_size * block_size];
+   // double * Alocal = new double[block_size * block_size];
+   // double * Blocal = new double[block_size * block_size];
+   // double * Clocal = new double[block_size * block_size];
 
    for (int i=0; i<n; i+=block_size){
       for (int j=0; j<n; j+=block_size){ 
@@ -58,7 +58,11 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
          }
       }
    }
-   delete[] Alocal;
-   delete[] Blocal;
-   delete[] Clocal;
+   delete Alocal;
+   delete Blocal;
+   delete Clocal;
+
+   // delete[] Alocal;
+   // delete[] Blocal;
+   // delete[] Clocal;
 }
