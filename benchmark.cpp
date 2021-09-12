2abe694386c0da54db8bca8bf396cd610f83901c
leonsanbo <kory.zhou@gmail.com>
2021-09-11 22:33:17 -0700

    accu

--- benchmark.cpp
+++ benchmark.cpp
@@ -55,7 +55,7 @@ int main(int argc, char** argv)
 {
     std::cout << "Description:\t" << dgemm_desc << std::endl << std::endl;
 
-    std::cout << std::fixed << std::setprecision(2);
+    std::cout << std::fixed << std::setprecision(5);
 
     std::vector<int> test_sizes{64, 128, 256, 512, 1024};
     std::vector<int> block_sizes{2, 16, 32, 64};
